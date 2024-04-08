e2e3c8e9445b9605cfed1c9cb69b3ce02c3e5a45
Karolina Seidlova <35819148+BcKarolinaSeidlova@users.noreply.github.com>
2024-04-08 02:16:40 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,4 +3,4 @@ var text = document.getElementById("text");
 var iframe = document.getElementById("myiframe");
    iframe.width = iframe.contentWindow.document.body.scrollWidth;
    iframe.height = iframe.contentWindow.document.body.scrollHeight;
-text.innerHTML = "width is: " + iframe.width;
+text.innerHTML = "width is " + iframe.width + " and height is " + iframe.height;
