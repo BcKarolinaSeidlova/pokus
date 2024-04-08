18f581e4b7fc436d33704ab5d8837358ff166fd0
Karolina Seidlova <35819148+BcKarolinaSeidlova@users.noreply.github.com>
2024-04-08 02:04:20 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,4 @@ text.innerHTML = "hello";
 var iframe = document.getElementById("myiframe");
    iframe.width = iframe.contentWindow.document.body.scrollWidth;
    iframe.height = iframe.contentWindow.document.body.scrollHeight;
+text.innerHTML = iframe.width;
