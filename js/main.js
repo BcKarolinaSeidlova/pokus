222f09247809819bd3d0bfb60fca174f1aa2bbd8
Karolina Seidlova <35819148+BcKarolinaSeidlova@users.noreply.github.com>
2024-04-25 21:29:36 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 var prvni = document.getElementById("Prvni");
-var DIV1 = document.getElementById("PrvniO");
+var DIV1 = document.getElementById("DIV1");
 
 var druhy = document.getElementById("Druhy");
-var DIV2 = document.getElementById("DruhyO");
+var DIV2 = document.getElementById("DIV2");
 
 var treti = document.getElementById("Treti");
-var DIV3 = document.getElementById("TretiO");
+var DIV3 = document.getElementById("DIV3");
 
+var DIV4 = document.getElementById("DIV4");
 
 prvni.addEventListener("click", Show.bind(DIV1, DIV2, DIV3));
 druhy.addEventListener("click", Show.bind(DIV2, DIV1, DIV3));
@@ -15,7 +16,7 @@ treti.addEventListener("click", Show.bind(DIV3, DIV1, DIV2));
 
 function Show (A, B, C) {
   if (A.style.getPropertyValue("display") == "none") {
-    
+    DIV4.innerHTML = A + B + C;
    A.style.setProperty("display", "flex");
    B.style.setProperty("display", "none");
    C.style.setProperty("display", "none");
