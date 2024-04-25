b15de6fb8ad3aeba89333f570e42cf8da206dc25
Karolina Seidlova <35819148+BcKarolinaSeidlova@users.noreply.github.com>
2024-04-25 22:50:25 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,19 +9,19 @@ var DIV3 = document.getElementById("DIV3");
 
 var DIV4 = document.getElementById("DIV4");
 
-prvni.addEventListener("click", Show.bind(DIV1, DIV2, DIV3));
-druhy.addEventListener("click", Show.bind(DIV2, DIV1, DIV3));
-treti.addEventListener("click", Show.bind(DIV3, DIV1, DIV2));
+prvni.addEventListener("click", Show.bind(1, 1, 1));
+druhy.addEventListener("click", Show.bind(2, 2, 2));
+treti.addEventListener("click", Show.bind(3, 3, 3));
 
 
 function Show (A, B, C) {
-  if (A.style.getPropertyValue("display") == "none") {
+ // if (A.style.getPropertyValue("display") == "none") {
     DIV4.innerHTML = A + B + C;
-   A.style.setProperty("display", "flex");
-   B.style.setProperty("display", "none");
-   C.style.setProperty("display", "none");
-} 
-  else {
-  A.style.setProperty("display", "none");
-  }
+ //  A.style.setProperty("display", "flex");
+//   B.style.setProperty("display", "none");
+//   C.style.setProperty("display", "none");
+//} 
+//  else {
+//  A.style.setProperty("display", "none");
+//  }
 }
