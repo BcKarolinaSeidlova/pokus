736cc2851af9647898effaf21f062852c058036c
Karolina Seidlova <35819148+BcKarolinaSeidlova@users.noreply.github.com>
2024-04-25 23:30:36 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,12 +7,13 @@ var DIV2 = document.getElementById("DIV2");
 var treti = document.getElementById("Treti");
 var DIV3 = document.getElementById("DIV3");
 
+var ctvrty = document.getElementById("Ctvrty");
 var DIV4 = document.getElementById("DIV4");
 
 prvni.addEventListener("click", Show.bind(null, DIV1, DIV2, DIV3));
 druhy.addEventListener("click", Show.bind(null, DIV2, DIV1, DIV3));
 treti.addEventListener("click", Show.bind(null, DIV3, DIV1, DIV2));
-
+ctvrty.addEventListerner("click", ShowAll);
 
 function Show (A, B, C) {
  if (A.style.getPropertyValue("display") == "none") {
@@ -24,4 +25,10 @@ function Show (A, B, C) {
  else {
    A.style.setProperty("display", "none");
   }
+}
+
+function ShowAll () {
+ DIV1.style.setProperty("display","flex");
+ DIV2.style.setProperty("display","flex");
+ DIV3.style.setProperty("display","flex");
 }
