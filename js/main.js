bcfee1a7337067c1908d3cd3a960f8cdc8da3441
Karolina Seidlova <35819148+BcKarolinaSeidlova@users.noreply.github.com>
2024-04-25 21:07:51 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
-var text = document.getElementById("text");
+var prvni = document.getElementById("Prvni");
+var prvniO = document.getElementById("PrvniO");
 
-var iframe = document.getElementById("myiframe");
-   iframe.width = iframe.contentWindow.document.body.scrollWidth;
-   iframe.height = iframe.contentWindow.document.body.scrollHeight;
-text.innerHTML = "width is " + iframe.width + " and height is " + iframe.height;
+var druhy = document.getElementById("Druhy");
+var druhyO = document.getElementById("DruhyO");
+
+var treti = document.getElementById("Treti");
+var tretiO = document.getElementById("TretiO");
+
+
+prvni.addEventListener("click", Show.bind(prvniO, druhyO, tretiO));
+druhy.addEventListener("click", Show.bind(druhyO, prvniO, tretiO));
+treti.addEventListener("click", Show.bind(tretiO, prvniO, druhyO));
+
+
+function Show (A, B, C) {
+  if (A.style.getPropertyValue("display") == "none") {
+    
+   A.style.setProperty("display", "flex");
+   B.style.setProperty("display", "none");
+   C.style.setProperty("display", "none");
+} 
+  else {
+  A.style.setProperty("display", "none");
+  }
+}
